e981522993b002b750482448969ffbeb2145345d
Alexander Gugel <alexander.gugel@gmail.com>
2014-04-25 02:57:59 +0200

    filter

--- src/underbar.js
+++ src/underbar.js
@@ -84,6 +84,13 @@ var _ = {};
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passed = [];
+    _.each(collection, function (element) {
+      if (test(element)) {
+        passed.push(element);
+      }
+    });
+    return passed;
   };
 
   // Return all elements of an array that don't pass a truth test.
