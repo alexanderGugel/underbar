059887c9088cdc10a692e4fdcd595ddfb7e2f6e2
Alexander Gugel <alexander.gugel@gmail.com>
2014-04-25 03:03:02 +0200

    map & pluck

--- src/underbar.js
+++ src/underbar.js
@@ -119,6 +119,11 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function (element, index) {
+      results[index] = iterator(element);
+    });
+    return results;
   };
 
   /*
