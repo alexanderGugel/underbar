faeeee352286f007696774bc19a7c9e8c86c42f3
Alexander Gugel <alexander.gugel@gmail.com>
2014-04-30 14:03:56 +0200

    clean up source

--- src/underbar.js
+++ src/underbar.js
@@ -62,7 +62,7 @@ var _ = {};
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
-  _.indexOf = function(array, target){
+  _.indexOf = function(array, target) {
     // TIP: Here's an example of a function that needs to iterate, which we've
     // implemented for you. Instead of using a standard `for` loop, though,
     // it uses the iteration helper `each`, which you will need to write.
