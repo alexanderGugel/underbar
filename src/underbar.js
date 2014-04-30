b35e8497d4e18396b833bcd89780f33274a4d52b
Alexander Gugel <alexander.gugel@gmail.com>
2014-04-30 14:00:04 +0200

    add semicolon

--- src/underbar.js
+++ src/underbar.js
@@ -39,7 +39,7 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    n>array.length && (n = array.length)
+    n > array.length && (n = array.length);
     return n === undefined ? array[array.length-1] : array.slice(array.length-n);
   };
 
